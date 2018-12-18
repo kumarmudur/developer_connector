a98785de8082573f1177b20224cec4fc81fbea1b
kumarmudur <kumarmudur10@gmail.com>
2018-12-19 00:53:55 +0530

    user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -52,4 +52,40 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route Get api/users/register
+// @desc  Login User / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+    console.log(req.body)
+    const {
+        email,
+        password
+    } = req.body;
+
+    // find user by email
+    User.findOne({
+        email
+    }).then(user => {
+        if (!user) {
+            return res.status(404).json({
+                email: 'User not found'
+            })
+        }
+
+        //check password
+        bcrypt.compare(password, user.password)
+            .then(isMatch => {
+                if (isMatch) {
+                    res.json({
+                        msg: 'Success'
+                    })
+                } else {
+                    return res.status(400).json({
+                        password: 'Password incorrect'
+                    });
+                }
+            })
+    });
+});
+
 module.exports = router;
